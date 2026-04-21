4ffcecf90831af45e6c0e142e65b9d3c82bac65b
GabrielS0ares <soaresgabriel33@gmail.com>
2026-04-21 08:58:34 -0300

    feat: utilizando os promts de saida em formatos predeterminado

--- main.py
+++ main.py
@@ -12,20 +12,6 @@ numero_dias = 4
 numero_criancas= 1
 atividade="contemplar a natureza"
 
-modelo_de_prompt = PromptTemplate(
-  template="""
-Crie um roteiro de viagem de {numero_dias} dias, para uma familia com {numero_criancas} de 1 ano, que gostam de {atividade}
-"""
-)
-
-# Aqui é onde alimenta a estrutura que foi usada para limitar o uso de tokens, assim fechando o contexto do uso
-prompt = modelo_de_prompt.format(
-  numero_dias=numero_dias,
-  numero_criancas=numero_criancas,
-  atividade=atividade
-)
-
-
 # Configuração do modelo, aqui poderia ser qualquer modelo que exista dentro do langchain para uso dos contructors
 modelo = ChatOpenAI(
   model="gpt-3.5-turbo",
@@ -33,15 +19,34 @@ modelo = ChatOpenAI(
   api_key=api_key
 )
 
+
+prompt_cidade = PromptTemplate(
+  """
+  Sugira uma cidade dado o meu interesse por {interesse}.
+  """,
+  input_variables=["interrese"]
+)
+
+# Criação de cadeias, é onde a configuração realmente acontecerá, utilizando dos modelos e regras de prompts que foram sugeridas
+cadeia = prompt_cidade | modelo | StrOutputParser
+
+modelo = ChatOpenAI(
+  model="gpt-3.5-turbo",
+  temperature=0.5,
+  api_key=api_key
+)
+
+resposta = cadeia.invoke({
+  "interesse" : "praias"
+})
+
+print(resposta.content)
+
+
+
 # Langchain tem um formato especifico para gerar a linguagem principal dos dados para gerar as cadeias de processos para que o fluxo da IA faça chamada especificas
 
 # A cadeia depende de três elementos: 1-Estrutura do promp que vai ser utilizado; 2-Modelo de LLM que será utilizado; 3-Formato que espero receber
 
 # Ter em mente o dormaro de saida é importe para ter o controle legitimo da saida dos dados
 
-
-resposta = modelo.invoke(prompt)
-
-print(resposta.content)
-
-print(resposta)
